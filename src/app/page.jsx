d5bfd4605fbbbc6f91b47373eb46deb8f3c3f191
OyuNet <oyunarda662@gmail.com>
2024-05-09 03:48:39 +0300

    Negative int problem fixed.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,47 +1,63 @@
-"use client"
-import { Button, TextField, ToggleButton, ToggleButtonGroup } from '@mui/material'
-import Image from 'next/image'
-import React from 'react'
+"use client";
+import {
+  Button,
+  TextField,
+  ToggleButton,
+  ToggleButtonGroup,
+} from "@mui/material";
+import React from "react";
 
 export default function Home() {
-  const [fileType, setFileType] = React.useState("gb")
-  const [downType, setDownType] = React.useState("mbit")
-  const [downSpeed, setDownSpeed] = React.useState("")
-  const [fileSize, setFileSize] = React.useState("")
-  const [resultVisibility, setResultVisibility] = React.useState(false)
+  const [fileType, setFileType] = React.useState("gb");
+  const [downType, setDownType] = React.useState("mbit");
+  const [downSpeed, setDownSpeed] = React.useState("");
+  const [fileSize, setFileSize] = React.useState("");
+  const [resultVisibility, setResultVisibility] = React.useState(false);
   const [timeAsSecond, setTimeAsSecond] = React.useState(0);
+  const [err, setErr] = React.useState(false);
+  const [errMessage, setErrMessage] = React.useState("");
 
   function calcTime() {
-    switch(fileType) {
-      case ("gb"):
-        switch(downType) {
-          case ("mbit"):
-            setTimeAsSecond(parseInt(fileSize)*1024/(parseInt(downSpeed)/8))
-            break
-          case ("gbit"):
-            setTimeAsSecond(parseInt(fileSize)*1024/((parseInt(downSpeed)*1024)/8))
-            break
+    if (parseInt(downSpeed) < 0 || parseInt(fileSize) < 0) {
+      setErr(true);
+      setErrMessage("Download speed or file size cannot be negative number.");
+    }
+    switch (fileType) {
+      case "gb":
+        switch (downType) {
+          case "mbit":
+            setTimeAsSecond(
+              (parseInt(fileSize) * 1024) / (parseInt(downSpeed) / 8),
+            );
+            break;
+          case "gbit":
+            setTimeAsSecond(
+              (parseInt(fileSize) * 1024) / ((parseInt(downSpeed) * 1024) / 8),
+            );
+            break;
           default:
-            setTimeAsSecond(0)
-            break
+            setTimeAsSecond(0);
+            break;
         }
-        break
-      case ("mb"):
-        switch(downType) {
-          case ("mbit"):
-            setTimeAsSecond(parseInt(fileSize)/(parseInt(downSpeed)/8))
-            break
-          case ("gbit"):
-            setTimeAsSecond(parseInt(fileSize)/((parseInt(downSpeed)*1024)/8))
-            break
+        break;
+      case "mb":
+        switch (downType) {
+          case "mbit":
+            setTimeAsSecond(parseInt(fileSize) / (parseInt(downSpeed) / 8));
+            break;
+          case "gbit":
+            setTimeAsSecond(
+              parseInt(fileSize) / ((parseInt(downSpeed) * 1024) / 8),
+            );
+            break;
           default:
-            setTimeAsSecond(0)
-            break
+            setTimeAsSecond(0);
+            break;
         }
-        break
+        break;
       default:
-        setTimeAsSecond(0)
-        break
+        setTimeAsSecond(0);
+        break;
     }
   }
 
@@ -111,24 +127,36 @@ export default function Home() {
         </Button>
       </div>
 
-      {resultVisibility && (
+      {!err && resultVisibility && (
         <div className="m-4 bg-gray-100 p-6 rounded-lg shadow-md">
           <h2 className="text-2xl font-bold text-center mb-4">Results!</h2>
           <div className="text-center">
             <p className="text-lg">
               Your approximately download time will be:{" "}
-              <span className="font-bold">{timeAsSecond.toFixed(0)}</span> seconds.
+              <span className="font-bold">{timeAsSecond.toFixed(0)}</span>{" "}
+              seconds.
             </p>
             <p className="text-lg">
               If we consider 10% speed loss then your download time will be:{" "}
               <span className="font-bold">
-                {(timeAsSecond * 10 / 9).toFixed(0)}
+                {((timeAsSecond * 10) / 9).toFixed(0)}
               </span>{" "}
               seconds.
             </p>
           </div>
         </div>
       )}
+
+      {err && (
+        <div className="m-4 bg-gray-100 p-6 rounded-lg shadow-md">
+          <h2 className="text-2xl font-bold text-center mb-4">Error!</h2>
+          <div className="text-center">
+            <p className="text-lg">
+              Error happened while calculating your time: {errMessage}
+            </p>
+          </div>
+        </div>
+      )}
     </main>
-  )
+  );
 }
